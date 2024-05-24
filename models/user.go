aabc5764c068513b11b064a24504b39e7c1ebc71
Permyakov-Dmitriy <dima.perm11@gmail.com>
2024-05-24 22:54:42 +0600

    change model user: username unique

--- models/user.go
+++ models/user.go
@@ -2,7 +2,7 @@ package models
 
 type User struct {
 	ID       uint   `json:"id" gorm:"primaryKey"`
-	Username string `json:"username" binding:"required"`
+	Username string `json:"username" gorm:"unique" binding:"required"`
 	Email    string `json:"email" gorm:"unique" binding:"required" validate:"email"`
 	Password string `json:"password"`
 }
