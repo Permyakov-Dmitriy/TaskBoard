6c1f8a686ad869873aaaa52604c33dc00bf4667d
Permyakov-Dmitriy <dima.perm11@gmail.com>
2024-05-24 22:55:55 +0600

    add user response model

--- models/user.go
+++ models/user.go
@@ -6,3 +6,9 @@ type User struct {
 	Email    string `json:"email" gorm:"unique" binding:"required" validate:"email"`
 	Password string `json:"password"`
 }
+
+type UserResponse struct {
+	ID    uint   `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
